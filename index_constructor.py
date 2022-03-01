9b908a0ca62eb135c15b8b3d06aa43dfc9d5bc89
David Trang <trangd3@uci.edu>
2022-02-28 18:05:03 -0800

    added tf-idf

--- index_constructor.py
+++ index_constructor.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient, InsertOne
 from pymongo.errors import BulkWriteError
 import json
 # import lxml
-# import math
+import math
 import nltk
 import os
 from bs4 import BeautifulSoup
@@ -24,40 +24,80 @@ tm['R'] = wordnet.ADV
 
 
 def index_constructor():
-    unique_words = defaultdict(lambda: defaultdict(float))
-    
+    num_docs = 0
+    tf = defaultdict(lambda: defaultdict(int))          # {docId: {lemma: term_count}}
+    df = defaultdict(int)                               # {word: document_count}
+    tf_idf = defaultdict(lambda: defaultdict(float))    # {word: {docId: score}}
+    # docs = defaultdict(lambda: defaultdict(list))
+
+    '''
+    word: {word: {docId: : score}}
+    docs: {docId: {"metadata": [], "title": [], "bold": [], h1: [], h2: [], h3: []}}
+    '''
+
     bk = json.load(open(f"{CORPUS_PATH}/bookkeeping.json"))
     for index, (id, url) in enumerate(bk.items()):
-        print(id)
+
         # testing cases
-        if index == 500:
+        print(id)
+        if index > 498:
             break;
+
         with open(f"{CORPUS_PATH}/{id}", "r", encoding="utf-8") as file:
-            text = file.read()
+            content = file.read()
 
             # ! https://stackoverflow.com/questions/56887086/validating-if-a-string-is-a-valid-html-in-python
-            if bool(BeautifulSoup(text, "lxml").find()):
-                t = BeautifulSoup(text, "lxml")
-                t = t.get_text().encode("ascii", "replace").decode().lower()
-                tokens = word_tokenize(t)
+            if bool(BeautifulSoup(content, "lxml").find()):
+                # soupify link and tokenize text
+                soup = BeautifulSoup(content, "lxml")
                 lemmatizer = WordNetLemmatizer()
+
+                text = soup.get_text().encode("ascii", "replace").decode().lower()
+                tokens = word_tokenize(text)
+                s = set()
 
                 # ! https://www.guru99.com/stemming-lemmatization-python-nltk.html
                 for token, tag in pos_tag(tokens):
-                    # * if token is not a stop word and is alphanumeric
-                    if len(token) >= 3 and token not in STOPWORDS and token.isalnum():
+                    # * if token is at least 3 characters long, not a stop word, and is alphanumeric
+                    if (len(token) >= 3 and token not in STOPWORDS and token.isalnum()) or token.isdigit():
+                        # lemmatize according to the token's first tag
                         lemma = lemmatizer.lemmatize(token, tm[tag[0]])
-                        if len(lemma) >= 3:
-                            unique_words[lemma][id] = index
 
+                        # finding term frequency for each doc (how many times it appears in this document)
+                        tf[id][lemma] += 1
+
+                        # finding document frequency (how many documents the lemma appears in)
+                        if lemma not in s:
+                            df[lemma] += 1
+                            s.add(lemma)
+
+                # for calculating df (total number of valid documents)
+                num_docs += 1
+
+                
+                # if (soup.title is not None):
+                #     title = soup.title.get_text().encode("ascii", "replace").decode().lower()
+                #     tokens = word_tokenize(title)
+                #     lst = []
+                #     for token in tokens:
+                #         lemma = lemmatizer.lemmatize(token)
+                #         lst.append(lemma)
     
     print("finished parsing")
+
+    for id in tf:
+        for lemma in tf[id]:
+            tf_idf[lemma][id] = (1+math.log(tf[id][lemma],10)) * math.log(num_docs/df[lemma])
+    
     try:
         # ! https://pymongo.readthedocs.io/en/stable/examples/bulk.html
-        db.example.bulk_write([InsertOne({"_id": word, "urls": unique_words[word]}) for word in unique_words])
+        db.test.bulk_write([InsertOne({"_id": term, "urls": tf_idf[term]}) for term in tf_idf])
+        # db.example.bulk_write([InsertOne({"_id": word, "urls": unique_words[word]}) for word in unique_words])
     except BulkWriteError as bwe:
         print(bwe.details)
-                              
+    
+    print("finished writing")
+
     return 0
 
 index_constructor()
